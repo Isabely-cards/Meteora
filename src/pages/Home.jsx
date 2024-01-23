5d1ee275de42a03d55961086f4edb6b6db2f5736
Isabely Cardoso Figueiredo <112524798+Isabely-cards@users.noreply.github.com>
2024-01-23 08:59:30 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,16 +2,24 @@ import BarraNavegacao from "../components/BarraNavegacao";
 import CarrinhoSuspenso from "../components/CarrinhoSuspenso";
 import Carrossel from "../components/Carrossel";
 import Categorias from "../components/Categorias";
+import Produtos from "../components/Produtos"
+import Facilidades from "../components/Facilidades"
+import Novidades from "../components/Novidades"
+import Rodape from "../components/Rodape"
 
 export default function Home () {
     return(
         <>
-            <BarraNavegacao/>
-            <CarrinhoSuspenso/>
-            <main>
-                <Carrossel/>
-                <Categorias/>
-            </main>
-        </>
+      <BarraNavegacao />
+      <CarrinhoSuspenso />
+      <main>
+        <Carrossel />
+        <Categorias />
+        <Produtos />
+        <Facilidades />
+        <Novidades />
+      </main>
+      <Rodape />
+    </>
     )
 }
